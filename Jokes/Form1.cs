40b421d7a05e2e70115cbd0e4195b178430e3a12
Golan1 <golanlevi1@gmail.com>
2016-11-13 21:46:07 +0200

    #hitchalnu

---
+++ Jokes/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Jokes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string oradb = "user id=conn;password=conn;data source=XE";
+
+            OracleConnection conn = new OracleConnection(oradb);
+            conn.Open();
+
+            string sql = " select 1 as x from dual";
+            OracleCommand cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+
+            var x = dr["x"].ToString();
+        }
+    }
+}
