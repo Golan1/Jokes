ff4de06a9e186b1edaa4dfd0a5fd86dd5861f0fb
bmn911 <bmn911@gmail.com>
2016-12-08 23:25:04 +0200

    updare jokes :+1:

--- Jokes/MainForm.cs
+++ Jokes/MainForm.cs
@@ -7,19 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DAL;
 
 namespace Jokes
 {
     public partial class MainForm : Form
     {
+
+        private Joke jokeForm;
+
         public MainForm()
         {
             InitializeComponent();
+
+            jokeForm = new Joke();
         }
 
         public void RefreshFiles()
         {
             this.filesTab1.RefreshFiles();
+        }
+
+        public void showJokes(string word, int selected, JokesDS.SEARCH_RESULTDataTable res)
+        {
+            jokeForm.updateJokes(word, selected, res);
+            jokeForm.Show();
         }
     }
 }
